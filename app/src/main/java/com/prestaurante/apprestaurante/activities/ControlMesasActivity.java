6dbbdf58347eb68941ea83f8e0eb28d4fa05e9ac
jledesma <jledesma2509@gmail.com>
2019-01-04 10:15:53 -0500

    Se agregaron:
    - Se paso el activity LoguinActivity a la carpeta activities
    - Activity para el control de mesas (ControlMesasActivity)
    - xml para el diseño de la lista personalizada (item_control_mesas.xml)

---
+++ app/src/main/java/com/prestaurante/apprestaurante/activities/ControlMesasActivity.java
@@ -0,0 +1,15 @@
+package com.prestaurante.apprestaurante.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.prestaurante.apprestaurante.R;
+
+public class ControlMesasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_mesas);
+    }
+}
